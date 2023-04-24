7de348b2e2d3a086572ac2cb9d1c4c236d11b3fe
Vlad <126405729+Mudoryu@users.noreply.github.com>
2023-04-24 15:14:03 +0300

    Add files via upload

---
+++ VS/JS.js
@@ -0,0 +1,6 @@
+alert("Hello World!"); 
+        alert("Glory to Ukraine!"); 
+        document.write("Hello World!")  //прописує документ
+        /*Багато рядковий коментар
+        рядок номер 2*/
+        
