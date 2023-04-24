499f2532bec3e5b2d179432686b135cfc267c34f
Vlad <126405729+Mudoryu@users.noreply.github.com>
2023-04-24 16:20:53 +0300

    Delete VS directory

--- VS/JS.js
+++
@@ -1,6 +0,0 @@
-alert("Hello World!"); 
-        alert("Glory to Ukraine!"); 
-        document.write("Hello World!")  //прописує документ
-        /*Багато рядковий коментар
-        рядок номер 2*/
-        
